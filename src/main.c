23f21361027042fabe8e44002a68430d4b1e950c
Ciro Cattuto <ciro.cattuto@gmail.com>
2022-10-15 16:56:55 +0200

    initial check-in

---
+++ src/main.c
@@ -0,0 +1,107 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Ciro Cattuto <ciro.cattuto@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "bsp/board.h"
+#include "tusb.h"
+#include "DAP.h"
+#include "hardware/gpio.h"
+
+// --------------------------------------------------------------------
+// MACRO CONSTANT TYPEDEF PROTYPES
+// --------------------------------------------------------------------
+
+void vendor_task(void);
+
+// main loop
+int main(void)
+{
+    board_init();
+    DAP_Setup();
+    tusb_init();
+    //vendord_init();
+
+    while (1) {
+        tud_task();     // tinyusb device task
+        vendor_task();  // WebUSB device task
+    }
+
+    return 0;
+}
+
+// --------------------------------------------------------------------
+// USB VENDOR (WEBUSB)
+// --------------------------------------------------------------------
+
+static uint8_t buf_rx[CFG_TUD_VENDOR_EPSIZE];
+static uint8_t buf_tx[CFG_TUD_VENDOR_EPSIZE];
+
+void vendor_task(void)
+{
+    uint32_t ret;
+    uint8_t size;
+
+    if ( tud_vendor_available() ) {
+        tud_vendor_read(&buf_rx, sizeof(buf_rx));
+        ret = DAP_ProcessCommand(buf_rx, buf_tx);
+        tud_vendor_write(&buf_tx, ret & 0xFFFF);
+    }
+}
+
+// --------------------------------------------------------------------
+// USB HID
+// --------------------------------------------------------------------
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
+{
+    // TODO not Implemented
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
+
+    return 0;
+}
+
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* RxDataBuffer, uint16_t bufsize)
+{
+    static uint8_t TxDataBuffer[CFG_TUD_HID_EP_BUFSIZE];
+
+    // This doesn't use multiple report and report ID
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+
+    DAP_ProcessCommand(RxDataBuffer, TxDataBuffer);
+
+    tud_hid_report(0, TxDataBuffer, sizeof(TxDataBuffer));
+}
